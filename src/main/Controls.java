77e59cae7aa0f76697ee843cba5e72e8dacff5bf
Nisarg Patel <63173283+Nisarg38@users.noreply.github.com>
2021-04-09 16:43:30 +0530

    Add files via upload

--- src/main/Controls.java
+++ src/main/Controls.java
@@ -10,7 +10,8 @@ import org.jogamp.java3d.WakeupOnAWTEvent;
 import org.jogamp.vecmath.Vector3f;
 import org.jogamp.java3d.WakeupCriterion;
 
-public class Controls extends Behavior {
+public class Controls extends Behavior{
+	
 	private TransformGroup targetTG;
 	private WakeupOnAWTEvent wEnter;
 	private float transY = 0.0f;
@@ -19,8 +20,8 @@ public class Controls extends Behavior {
 	public Controls(TransformGroup targetTG) {
 		this.targetTG = targetTG;
 	}
-
-	/// set initial wakeup condition
+	
+	//Set initial wakeup condition
 	public void initialize() {
 		wEnter = new WakeupOnAWTEvent(MouseEvent.MOUSE_PRESSED);
 		wakeupOn(wEnter);
@@ -28,11 +29,15 @@ public class Controls extends Behavior {
 
 	@Override
 	public void processStimulus(Iterator<WakeupCriterion> criteria) {
+		
 		if (++transY > 6.0f) {
 			transY = -2.0f;
 		}
-		trans.set(new Vector3f(0.0f, transY / 10.0f, 0.0f));
+		
+		trans.set(new Vector3f(0.0f, transY/10.0f, 0.0f));
 		targetTG.setTransform(trans);
-		wakeupOn(wEnter);
+		wakeupOn(wEnter);	
+		
 	}
+	
 }
